b9d20091b322f4b914bd062d18487792cb8ee083
da-lbrecht <david.albrecht@protonmail.com>
2025-02-27 15:55:05 +0100

    Run stata runner for 5% and 95% samples one after another.

--- stata_runner_app_95.py
+++ stata_runner_app_95.py
@@ -38,7 +38,7 @@ def rename_csv_file(do_file_name):
         print(f"CSV directory {CSV_DIR} not found.")
         return
     for file_name in os.listdir(CSV_DIR):
-        if file_name.startswith("hypothesis-") and "_results.csv" in file_name:
+        if file_name.startswith("h") and "_results.csv" in file_name:
             new_file_name = f"repro_{do_file_name}_{file_name}"
             os.rename(os.path.join(CSV_DIR, file_name), os.path.join(CSV_DIR, new_file_name))
             print(f"Renamed {file_name} to {new_file_name}")
